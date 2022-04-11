3d5c136c27b130d525af727a9b77a5a92823846c
CristhianOva <chrisov9@gmail.com>
2022-04-10 22:34:45 -0400

    somo lo fuerte

--- holamundo/src/App.js
+++ holamundo/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import react from "react"
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <a href="../" ></a>
+      <a href="https://www.google.com/webhp?hl=es-419&sa=X&ved=0ahUKEwjv__bVzYP3AhXsQjABHdZuAV8QPAgI" > google </a>
     </div>
   );
 }
